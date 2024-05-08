f1852ced531bf35a9afec4838aa9ea3b787864f0
Joshua <josuealejandrof926@gmail.com>
2024-05-09 00:20:07 +0200

    Working linter

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,7 +10,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 // Home route
 app.get('/', (req, res) => {
   res.render('home');
