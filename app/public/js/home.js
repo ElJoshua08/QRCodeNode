f063236f171f0f40b409ec7094273102d80ccb30
Joshua <josuealejandrof926@gmail.com>
2024-05-11 15:59:02 +0200

    Update home.js, home.ejs and home.css: add QR code container, display QR code on button click, and update CSS styles
    The changes include:
    
    * In home.js, add a QR

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -7,10 +7,10 @@ $createQRButton.addEventListener('click', async () => {
     alert('Please add a text or an URL');
     return;
   }
-  
+
   let dataToSend = {
     content: $textInput.value,
-  }
+  };
 
   const res = await fetch(`/qr`, {
     method: 'POST',
@@ -19,16 +19,22 @@ $createQRButton.addEventListener('click', async () => {
     },
     body: JSON.stringify(dataToSend),
   });
-  
+
   const receivedData = await res.json();
 
   if (receivedData.status === 'OK') {
     const qrCode = new Image();
     qrCode.src = receivedData.content;
+    qrCode.classList.add('qr');
 
-    document.body.appendChild(qrCode);
+    const $qrContainer = document.querySelector('.qr-container');
+    $qrContainer.appendChild(qrCode);
+    const $qrs = $qrContainer.querySelectorAll('.qr');
+
+
+    if ($qrs.length > 0) {
+      $qrContainer.classList.add('active');
+    }
   } else {
-    console.log(receivedData.message)
   }
-  console.log(receivedData);
 });
